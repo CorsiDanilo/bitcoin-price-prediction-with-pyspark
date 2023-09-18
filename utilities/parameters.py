da4defa4285bdcc1e97ec3fb322f11e79d05beee
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2023-09-18 18:36:00 +0200

    Fixing splits notebook + hyp choosing function eng

--- utilities/parameters.py
+++ utilities/parameters.py
@@ -28,9 +28,9 @@ def get_splitting_params(split_type):
 '''
 Description: Returns the default parameters of the selected model
 Args:
-    model_name: Model name selected
+    model_name: Name of the selected model
 Return: 
-    params: Parameters of the selected model
+    params: Parameters list of the selected model
 '''
 def get_defaults_model_params(model_name):
     if (model_name == 'LinearRegression'):
@@ -63,9 +63,9 @@ def get_defaults_model_params(model_name):
 '''
 Description: Returns the model grid parameters of the selected model
 Args:
-    model_name: Model name selected
+    model_name: Name of the selected model
 Return: 
-    params: Parameters of the selected model
+    params: Parameters list of the selected model
 '''
 def get_model_grid_params(model_name):
     if (model_name == 'LinearRegression'):
@@ -98,38 +98,86 @@ def get_model_grid_params(model_name):
     return params
 
 '''
+Description: Returns the best model parameters
+Args:
+    parameters: Name of the selected model
+Return: 
+    grouped_scores: Scores of all parameters
+    best_params: Parameters with the best score
+'''
+def choose_best_params(parameters):
+    # Calculate the weight of each value in the "Splits" column
+    parameters['Split weight'] = parameters['Splits'].rank(ascending=True)
+
+    # Normalize the split weights to a scale of 0 to 1
+    parameters['Split weight'] = parameters['Split weight'] / parameters['Split weight'].max()
+    
+    # Normalize the RMSE values to a scale of 0 to 1
+    rmse_weight = 1 - (parameters['RMSE'] / parameters['RMSE'].max())
+
+    # Add the RMSE weight as a new column
+    parameters['RMSE weight'] = rmse_weight
+    
+    # To access the values of a tuple 
+    parameters['Parameters'] = parameters['Parameters'].apply(tuple)
+
+    # Calculate the frequency of each unique value in the "Parameters" column
+    freq = parameters['Parameters'].value_counts(normalize=True)
+
+    # Normalize the frequencies to a scale of 0 to 1
+    freq_norm = freq / freq.max()
+
+    # create a new column called "Frequency" with the normalized frequencies
+    parameters['Frequency weight'] = parameters['Parameters'].map(freq_norm)
+
+    # Group the rows by the "Parameters" column and calculate the average of the other columns
+    grouped_scores = parameters[['Parameters', 'Split weight', 'RMSE weight', "Frequency weight"]].groupby('Parameters').mean()
+    
+    # Calculate the final score for each row
+    grouped_scores['Final score'] = grouped_scores['Frequency weight'] *  grouped_scores['Split weight'] * grouped_scores['RMSE weight']
+
+    # Sort the DataFrame by the "Final score" column in descending order
+    grouped_scores = grouped_scores.sort_values(by='Final score', ascending=False)
+
+    # Get the index of the row with the highest final score
+    best_params = grouped_scores['Final score'].idxmax()
+
+    return grouped_scores, best_params
+
+'''
 Description: Returns the best model parameters of the selected model
 Args:
-    model_name: Model name selected
+    parameters: Parameters to be entered in the parameter grid of the selected model
+    model_name: Name of the selected model
 Return: 
-    params: Parameters of the selected model
+    params: Parameters list of the selected model
 '''
-def get_best_model_params(model_name):
+def get_best_model_params(parameters, model_name):
     if (model_name == 'LinearRegression'):
         params = {
-            'maxIter' : [5],
-            'regParam' : [0.0],
-            'elasticNetParam' : [0.0]
+            'maxIter' : [parameters[0]],
+            'regParam' : [parameters[1]],
+            'elasticNetParam' : [parameters[2]]
         }   
     if (model_name == 'GeneralizedLinearRegression'):
         params = {
-            'maxIter' : [5],
-            'regParam' : [0.2],
-            'family': ['gaussian'],
-            'link': ['log']
+            'maxIter' : [parameters[0]],
+            'regParam' : [parameters[1]],
+            'family': [parameters[2]],
+            'link': [parameters[3]]
         }
     elif (model_name == 'RandomForestRegressor'):
         params = {
-            'numTrees' : [10],
-            'maxDepth' : [10],
-            'seed' : [RANDOM_SEED]
+            'numTrees' : [parameters[0]],
+            'maxDepth' : [parameters[1]],
+            'seed' : [parameters[2]]
             }
     elif (model_name == 'GradientBoostingTreeRegressor'):
         params = {
-            'maxIter' : [30],
-            'maxDepth' : [3],
-            'stepSize': [0.4],
-            'seed' : [RANDOM_SEED]
+            'maxIter' : [parameters[0]],
+            'maxDepth' : [parameters[1]],
+            'stepSize': [parameters[2]],
+            'seed' : [parameters[3]]
         }
         
     return params
